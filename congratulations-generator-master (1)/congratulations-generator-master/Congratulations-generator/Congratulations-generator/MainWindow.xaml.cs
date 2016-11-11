26b0afc53aae8ac6f06a4eedfa255e72ead72a4a
TenshowV <hatefulpain@mail.ru>
2016-11-12 00:14:11 +0300

    Привязал БД и проинициализировал поля главной формы.

---
+++ congratulations-generator-master (1)/congratulations-generator-master/Congratulations-generator/Congratulations-generator/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Congratulations_generator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static void insertToDataBase(List <Human> lct)
+        {
+            humanBaseEntities context = new humanBaseEntities();
+            foreach (Human ct in lct)
+                context.Human.Add(ct);
+
+            context.SaveChanges();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            createHobbiesComboBox();
+            createcomboBoxHoliday();
+            createtextBoxAge();
+            Img1.Source = new BitmapImage(new Uri("Images/top.png", UriKind.Relative));
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private int returnLastId()
+        {
+            humanBaseEntities context = new humanBaseEntities();
+            IQueryable<Human> custs = context.Human
+                                                   .OrderByDescending(c => c.Id)
+                                                   .Select(c => c);
+            return custs.First().Id;
+        }
+
+        private void createHobbiesComboBox()
+        {
+            humanBaseEntities context = new humanBaseEntities();
+            IQueryable<Interests> custs = context.Interests
+                .Select(c => c);
+            foreach (Interests cust in custs)
+            {
+                comboBoxInterests.Items.Add(cust.Name);
+            }
+            //MessageBox.Show(cust.Name);
+        }
+
+        private void createcomboBoxHoliday()
+        {
+            humanBaseEntities context = new humanBaseEntities();
+            IQueryable<Holiday> custs = context.Holiday
+                .Select(c => c);
+            foreach (Holiday cust in custs)
+            {
+                comboBoxHoliday.Items.Add(cust.Name);
+            }
+            //MessageBox.Show(cust.Name);
+        }
+
+        private void createtextBoxAge()
+        {
+            for (int i = 3; i <= 100; i++) textBoxAge.Items.Add(i);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Human> lct = new List<Human>
+             {
+                 new Human
+                 {
+                     Id=returnLastId()+1,
+                     Name=textBoxName.Text,
+                     Solute=textBoxCall.Text
+                 }
+             };
+            MainWindow.insertToDataBase(lct);
+
+            MessageBox.Show(textBoxName.Text);
+            //clearFun();
+
+            OutputWindow outputWindow = new OutputWindow();
+            //место для функции корректировки данных
+            //место для функции генирации стартового поздравления и картинок
+            //не забыть закрыть первую форму при корректном заполнении
+            outputWindow.richTextBox.AppendText("Hello,World!\n");
+            outputWindow.richTextBox.AppendText("Hello,Life!\n\n\n\n\n\n\n\n\n\n");
+            outputWindow.richTextBox.AppendText("Hello,Friends!\n");
+            outputWindow.Show();
+        }
+    }
+}
---
+++ congratulations-generator-master (1)/congratulations-generator-master/Congratulations-generator/Congratulations-generator/OutputWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Congratulations_generator
+{
+    /// <summary>
+    /// Логика взаимодействия для OutputWindow.xaml
+    /// </summary>
+    public partial class OutputWindow : Window
+    {
+
+        public OutputWindow()
+        {
+            InitializeComponent();
+            Image1.Source = new BitmapImage(new Uri("Images/pepe1.jpg", UriKind.Relative));
+            Image2.Source = new BitmapImage(new Uri("Images/pepe2.png", UriKind.Relative));
+            Image3.Source = new BitmapImage(new Uri("Images/pepe3.png", UriKind.Relative));
+            Image4.Source = new BitmapImage(new Uri("Images/pepe4.jpg", UriKind.Relative));
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonPast_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
