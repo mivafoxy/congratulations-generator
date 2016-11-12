25884350ad324a5c19adc28826ad28354a8f74b5
velichkomarija <velichkomarija4@gmail.com>
2016-11-13 02:39:55 +0300

    Изменен дизайн окон, добавлено еще одно окно, так как был предложен дополнительный функционал.

--- Congratulations-generator/Congratulations-generator/OutputWindow.xaml.cs
+++ Congratulations-generator/Congratulations-generator/OutputWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Congratulations_generator
 {
@@ -66,7 +57,7 @@ namespace Congratulations_generator
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+            outputWindow.Close();
         }
     }
 }
